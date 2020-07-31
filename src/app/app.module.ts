aa10a03493ebf382cfe433083ac58ee38b537cf5
Admin <ondianeveah19@gmail.com>
2020-07-31 09:38:03 -0400

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from '../app/components/create-employee/create-employee.component';
+import { EmployeeListComponent } from '../app/components/employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from '../app/components/employee-details/employee-details.component';
+import { UpdateEmployeeComponent } from '../app/components/update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeListComponent,
+    EmployeeDetailsComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
